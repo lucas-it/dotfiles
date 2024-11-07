73535db469235b73724c921fe470601724c5fb5e
lucas-it <32235434+lucas-it@users.noreply.github.com>
2024-11-07 21:48:37 +0100

    better nvim cmp config

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,10 +1,10 @@
 require "nvchad.options"
 
 -- Tabs vs spaces
-vim.o.tabstop = 2      -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 2  -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 2   -- Number of spaces inserted when indenting
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 -- Enable mouse mode
 vim.o.mouse = "a"
@@ -27,3 +27,6 @@ vim.wo.signcolumn = "yes"
 
 -- Enable terminal colors
 vim.o.termguicolors = true
+
+-- Scroll offset when navigating with cursor in file
+vim.o.scrolloff = 6
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -105,7 +105,16 @@ return {
   },
 
   {
-    "lewis6991/gitsigns.nvim",
-    enabled = false,
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    opts = function()
+      return require "configs.cmp"
+    end,
   },
 }
