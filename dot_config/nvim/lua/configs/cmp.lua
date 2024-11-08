a4b32af739255a7bc0f9cc74e6053e3a7f905b86
lucas-it <32235434+lucas-it@users.noreply.github.com>
2024-11-08 16:44:12 +0100

    cmp config

---
+++ dot_config/nvim/lua/configs/cmp.lua
@@ -0,0 +1,41 @@
+vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
+local cmp = require "cmp"
+local defaults = require "nvchad.configs.cmp"
+
+return {
+  auto_brackets = {},
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+  },
+  preselect = cmp.PreselectMode.Item,
+  mapping = defaults.mapping,
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "path" },
+  }, {
+    { name = "buffer" },
+  }),
+  formatting = {
+    format = function(_, item)
+      local widths = {
+        abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+        menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+      }
+
+      for key, width in pairs(widths) do
+        if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+          item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+        end
+      end
+
+      return item
+    end,
+  },
+  experimental = {
+    ghost_text = {
+      hl_group = "CmpGhostText",
+    },
+  },
+  sorting = defaults.sorting,
+}
